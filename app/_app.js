9e69971cc5a04f90e87a7da54c8b6bada9a5bedd
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-10-10 10:31:52 +0200

    init

---
+++ app/_app.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+
+function App() {
+  return (
+    <NextUIProvider>
+      <YourApplication />
+    </NextUIProvider>
+  );
+}
+
+export default App;
